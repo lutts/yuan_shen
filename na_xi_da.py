b3253ea92e4dcc5c240cd01be31e1f46c507914a
Lutts <lutts.cao@gmail.com>
2023-11-24 00:31:29 +0800

    refactor

--- na_xi_da.py
+++ na_xi_da.py
@@ -10,12 +10,14 @@ import logging
 import itertools
 from base_syw import ShengYiWu, calculate_score, find_syw
 
+
 def match_sha_callback(syw: ShengYiWu):
     return syw.elem_mastery == ShengYiWu.ELEM_MASTERY_MAIN
 
 
 def match_bei_callback(syw: ShengYiWu):
-    return syw.elem_type == ShengYiWu.ELEM_TYPE_CAO # or syw.atk_per == ShengYiWu.BONUS_MAX or syw.elem_mastery == ShengYiWu.ELEM_MASTERY_MAIN
+    # or syw.atk_per == ShengYiWu.BONUS_MAX or syw.elem_mastery == ShengYiWu.ELEM_MASTERY_MAIN
+    return syw.elem_type == ShengYiWu.ELEM_TYPE_CAO
 
 
 def match_syw(s: ShengYiWu, expect_name):
@@ -29,20 +31,25 @@ def match_syw(s: ShengYiWu, expect_name):
     else:
         return True
 
+
 def find_combins_callback():
-    shen_lin = find_syw(match_syw_callback=lambda s: match_syw(s, ShengYiWu.SHEN_LIN))
+    shen_lin = find_syw(
+        match_syw_callback=lambda s: match_syw(s, ShengYiWu.SHEN_LIN))
     return list(itertools.combinations(shen_lin, 4))
 
-def calculate_score_callback(combine : list[ShengYiWu]):
+
+def calculate_score_callback(combine: list[ShengYiWu]):
     wu_qi_atk = 542
     base_atk = 299
     bai_zhi_atk = wu_qi_atk + base_atk
 
-    wu_qi_elem_mastery = 265
-    tu_po_elem_mastery = 115
-    ming_4_min_elem_mastery = 100
-    q_extra_elem_mastery = 243  # 固有天赋，阿忍带圣显，四饰金加的150可以被纳西妲大招转化
-    sheng_xian_elem_mastery = 76    # 久岐圣显
+    extra_elem_mastery = {
+        "wu qi": 265,
+        "deng ji tu po": 115,
+        "4 ming": 100,
+        "q from jiu qi ren": 243,  # 固有天赋，阿忍带圣显，四饰金加的150可以被纳西妲大招转化
+        "sheng xian": 76    # 久岐圣显
+    }
 
     shen_lin_elem_bonus = 0.15      # 草套2件套效果
     ming_1_elem_bonus = 0.316       # 1命
@@ -50,8 +57,8 @@ def calculate_score_callback(combine : list[ShengYiWu]):
 
     panel_crit_rate = 0.05
     crit_damage = 1 + 0.5
-    elem_mastery = wu_qi_elem_mastery + tu_po_elem_mastery + ming_4_min_elem_mastery + q_extra_elem_mastery + sheng_xian_elem_mastery
-    panel_elem_mastery = wu_qi_elem_mastery + tu_po_elem_mastery
+    elem_mastery = sum(extra_elem_mastery.values())
+    panel_elem_mastery = extra_elem_mastery["wu qi"] + extra_elem_mastery["deng ji tu po"]
     elem_bonus = shen_lin_elem_bonus + ming_1_elem_bonus + wu_qi_elem_bonus
     atk = 311
     atk_per = 0
@@ -70,7 +77,7 @@ def calculate_score_callback(combine : list[ShengYiWu]):
     crit_rate = panel_crit_rate + + min((elem_mastery - 200) * 0.0003, 0.24)
     if crit_rate < 0.64:
         return None
-    
+
     elem_bonus += min((elem_mastery - 200) * 0.001, 0.8)
     all_atk = int(bai_zhi_atk * (1 + atk_per)) + atk
     energy_recharge = (energy_recharge + 1) * 100
@@ -80,12 +87,13 @@ def calculate_score_callback(combine : list[ShengYiWu]):
     expect_score = non_crit_score * (1 + crit_rate * (crit_damage - 1))
     return [expect_score, crit_score, elem_mastery, panel_elem_mastery, int(all_atk), round(panel_crit_rate, 3), round(crit_rate, 3), round(crit_damage - 1, 3), round(energy_recharge, 3), combine]
 
+
 def find_syw_for_na_xi_da():
     return calculate_score(find_combine_callback=find_combins_callback,
-                    match_sha_callback=match_sha_callback,
-                    match_bei_callback=match_bei_callback,
-                    calculate_score_callbak=calculate_score_callback,
-                    result_txt_file="na_xi_da_syw.txt")
+                           match_sha_callback=match_sha_callback,
+                           match_bei_callback=match_bei_callback,
+                           calculate_score_callbak=calculate_score_callback,
+                           result_txt_file="na_xi_da_syw.txt")
 
 
 # Main body
