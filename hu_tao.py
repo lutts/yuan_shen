537fcd6d04698e7240903bcb218cbaa267f95c74
Lutts <lutts.cao@gmail.com>
2023-11-24 01:03:06 +0800

    refator

--- hu_tao.py
+++ hu_tao.py
@@ -69,23 +69,47 @@ def find_combins_callback():
 
     return all_combins
 
-def calculate_score_callback(combine):
+def calculate_score_callback(combine: list[ShengYiWu]):
     base_hp = 15552
-    base_atk = 715
-    hu_mo_crit_damage = 0.662
-    base_crit_damage = 1 + 0.884 + hu_mo_crit_damage   # 突破加暴伤
-    ye_lan_bonus = 0.29 # 平均增伤
-    shui_shen_bonus = 1.24
-    base_elem_bonus = 1 + ye_lan_bonus + shui_shen_bonus
 
-    crit_rate = sum([p.crit_rate for p in combine]) + 0.05
-    atk = sum([p.atk for p in combine]) + 311
-    atk_per = sum([p.atk_per for p in combine])
-    hp = sum([p.hp for p in combine])
-    hp_per = sum([p.hp_percent for p in combine])
-    extra_crit_damage = sum([p.crit_damage for p in combine])
-    extra_elem_bonus = sum([p.elem_bonus for p in combine])
-    elem_mastery = sum([p.elem_mastery for p in combine])
+    base_atk = 107
+    wu_qi_atk = 608 # 护摩
+    bai_zhi_atk = base_atk + wu_qi_atk
+
+    extra_crit_damage = {
+        "等级突破": 0.884,
+        "护摩": 0.662
+    }
+
+    extra_elem_bonus = {
+        "生命低于50%时的固有天赋": 0.33,
+        "夜兰平均增伤": 0.29,
+        "水神增伤": 1.24,
+    }
+
+    extra_hp_bonus = {
+        "护摩": 0.2,
+        "双水": 0.25,
+    }
+
+    crit_rate = 0.05
+    crit_damage = 1 + sum(extra_crit_damage.values())
+    atk = 311
+    atk_per = 0
+    hp = 0
+    hp_per = sum(extra_hp_bonus.values())
+    elem_bonus = 1 + sum(extra_elem_bonus.values())
+    elem_mastery = 0
+
+    for p in combine:
+        crit_rate += p.crit_rate
+        crit_damage += p.crit_damage
+        atk += p.atk
+        atk_per += p.atk_per
+        hp += p.hp
+        hp_per += p.hp_percent
+        elem_bonus += p.elem_bonus
+        elem_mastery += p.elem_mastery
 
     if elem_mastery < 200:
         return None
@@ -101,11 +125,11 @@ def calculate_score_callback(combine):
         elif n == ShengYiWu.ZHUI_YI:
             atk_per += 0.18
             if name_count[n] >= 4:
-                extra_elem_bonus += 0.5
+                elem_bonus += 0.5
         elif n == ShengYiWu.JUE_DOU_SHI:
             atk_per += 0.18
         elif n == ShengYiWu.LIE_REN:
-            extra_elem_bonus += 0.15
+            elem_bonus += 0.15
             if name_count[n] >= 4:
                 crit_rate += 0.36
 
@@ -113,19 +137,14 @@ def calculate_score_callback(combine):
         return None
     
     # 按生命低于50%血时11次az的伤害计算
-    hp_per += 0.2 # 护摩
-    panel_hp = int(base_hp * (1 + hp_per)) + hp + 4780
-    hp_per += 0.25   # 双水
+    panel_hp = int(base_hp * (1 + hp_per - extra_hp_bonus["双水"])) + hp + 4780
     all_hp = int(base_hp * (1 + hp_per)) + hp + 4780
 
     hu_mo_atk_bonus = int(all_hp * 0.018)  # 1.8%
     e_atk_bonus = int(all_hp * 0.0626)   # 6.26/5
-    if e_atk_bonus >= base_atk * 4: # 不超过基础攻击力的400%
-        e_atk_bonus = base_atk * 4
-    all_atk = int(base_atk * (1 + atk_per)) + atk + hu_mo_atk_bonus + e_atk_bonus
-    
-    elem_bonus = base_elem_bonus + extra_elem_bonus + 0.33  # 低于50%时的固有天赋0.33
-    crit_damage = base_crit_damage + extra_crit_damage
+    if e_atk_bonus >= bai_zhi_atk * 4: # 不超过基础攻击力的400%
+        e_atk_bonus = bai_zhi_atk * 4
+    all_atk = int(bai_zhi_atk * (1 + atk_per)) + atk + hu_mo_atk_bonus + e_atk_bonus
 
     # 11次az
     az_damage = all_atk * (69.3 + 200.9) / 100 * elem_bonus * 11
