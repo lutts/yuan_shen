02ec23d28ca587ff9ec06db7ce080506c569243d
Lutts <lutts.cao@gmail.com>
2023-11-25 12:59:29 +0800

    add cang gu

--- ai_er_hai_seng.py
+++ ai_er_hai_seng.py
@@ -51,25 +51,34 @@ def find_combins_callback():
             for l in list(itertools.combinations(all_combines_2, 2))] + shi_jin_4
 
 
-def calculate_score_callback(combine : list[ShengYiWu]):
+def calculate_score_callback(combine: list[ShengYiWu]):
     base_atk = 314
-    wu_qi_atk = 608 # 波波刀
+    wu_qi_atk = 608  # 波波刀/苍古
     bai_zhi_atk = base_atk + wu_qi_atk
+
     na_xi_da_zhuan_wu_elem_mastery = 40
     shuang_cao_elem_mastery = 50 + 50   # 双草共鸣
-    sheng_xian_mastery = 76 # 久岐忍圣显
+    sheng_xian_mastery = 78  # 久岐忍圣显
+
     base_elem_bonus = 1 + 0.288  # 等级突破
+
     bo_bo_dao_bonus = 0.12
     bo_bo_dao_a_bonus = 2 * 0.2     # 波波刀2层
-    bo_bo_dao_crit_rate = 0.331
+    bo_bo_dao_crit_rate =0.331
+
+    cang_gu_mastery = 0 #198
+    cang_gu_elem_bonus = 0 #0.1    # 苍古
+    cang_gu_a_bonus = 0 #0.16
+    cang_gu_atk_per = 0 #0.2
 
     crit_rate = 0.05 + bo_bo_dao_crit_rate
     crit_damage = 1 + 0.5
-    elem_mastery = na_xi_da_zhuan_wu_elem_mastery + shuang_cao_elem_mastery + sheng_xian_mastery
+    elem_mastery = na_xi_da_zhuan_wu_elem_mastery + \
+        shuang_cao_elem_mastery + sheng_xian_mastery + cang_gu_mastery
     panel_elem_mastery = 0
     atk = 311
-    atk_per = 0
-    elem_bonus = base_elem_bonus + bo_bo_dao_bonus
+    atk_per = cang_gu_atk_per
+    elem_bonus = base_elem_bonus + bo_bo_dao_bonus + cang_gu_elem_bonus
 
     for p in combine:
         crit_rate += p.crit_rate
@@ -83,9 +92,9 @@ def calculate_score_callback(combine : list[ShengYiWu]):
     crit_rate = round(crit_rate, 3)
     if crit_rate < 0.65:
         return None
-    
+
     panel_atk_per = atk_per
-    
+
     syw_names = [p.name for p in combine]
     # print(syw_names)
     name_count = {i: syw_names.count(i) for i in syw_names}
@@ -106,26 +115,38 @@ def calculate_score_callback(combine : list[ShengYiWu]):
             panel_elem_mastery += 80
         elif n == ShengYiWu.SHEN_LIN:
             elem_bonus += 0.15
-    
+
     # 固有天赋2
     elem_bonus += min(elem_mastery * 0.001, 1)
-    
-    a_elem_bonus = elem_bonus + bo_bo_dao_a_bonus
+
+    a_elem_bonus = elem_bonus + bo_bo_dao_a_bonus + cang_gu_a_bonus
     all_atk = int(bai_zhi_atk * (1 + atk_per)) + atk
     panel_atk = int(bai_zhi_atk * (1 + panel_atk_per)) + atk
 
     # 以下计算基于以下手法
     # Q 切久岐忍e 再切回海哥 aaa闪 aaa e aaa闪 aaa z aaa aaa，共计六发满层光幕攻击
     # 波波刀的普攻增伤需要海哥e之后才有，能覆盖开e后续所有的平A
-    q_damage = (all_atk * 218.9 / 100 + elem_mastery * 175.1 / 100) * elem_bonus
+    q_damage = (all_atk * 218.9 / 100 +
+                elem_mastery * 175.1 / 100) * elem_bonus
 
     aaa_damage = all_atk * (91 + 93.2 + 62.8 + 62.8) / 100
     z_damage = all_atk * (101.5 + 101.5) / 100
 
-    e_damage = (all_atk * 3.485 / 100 + elem_mastery * 2.788 / 100) * elem_bonus
-    guang_mu_damage = (all_atk * 121 / 100 + elem_mastery * 241.9 / 100) * 3 * elem_bonus * 6  # 六剑雨
+    e_damage = (all_atk * 3.485 / 100 +
+                elem_mastery * 2.788 / 100) * elem_bonus
+    guang_mu_damage = (all_atk * 121 / 100 + elem_mastery *
+                       241.9 / 100) * 3 * elem_bonus * 6  # 六剑雨
 
-    non_crit_score = q_damage + 2 * aaa_damage * elem_bonus + e_damage + z_damage + 4 * aaa_damage * a_elem_bonus + guang_mu_damage
+    if cang_gu_a_bonus != 0:
+        a_without_extra_bonus_num = 0
+        a_with_extra_bonus_num = 6
+    else:
+        a_without_extra_bonus_num = 2
+        a_with_extra_bonus_num = 4
+
+    non_crit_score = q_damage + a_without_extra_bonus_num * aaa_damage * elem_bonus + \
+        e_damage + z_damage + a_with_extra_bonus_num * \
+        aaa_damage * a_elem_bonus + guang_mu_damage
     crit_score = non_crit_score * crit_damage
     expect_score = non_crit_score * (1 + crit_rate * (crit_damage - 1))
 
