90d18df1b4b61190551d5cba6142649f660a519e
Lutts <lutts.cao@gmail.com>
2023-11-27 23:52:33 +0800

    add ming zuo for ye lana

--- ye_lan.py
+++ ye_lan.py
@@ -10,8 +10,10 @@ import logging
 import itertools
 from base_syw import ShengYiWu, find_syw, calculate_score
 
+ming_zuo_num = 6
+
 def match_sha_callback(syw:ShengYiWu):
-    return syw.hp_percent == 0.466
+    return syw.hp_percent == 0.466 or syw.energe_recharge == ShengYiWu.ENERGE_RECHARGE_MAX
 
 
 def match_bei_callback(syw:ShengYiWu):
@@ -34,16 +36,19 @@ def find_combine_callback():
     qian_yan = find_syw(match_syw_callback=lambda s: match_syw(s, ShengYiWu.QIAN_YAN))
     chen_lun = find_syw(match_syw_callback=lambda s: match_syw(s, ShengYiWu.CHEN_LUN))
     shui_xian = find_syw(match_syw_callback=lambda s: match_syw(s, ShengYiWu.SHUI_XIAN))
+    jue_yuan = find_syw(match_syw_callback=lambda s: match_syw(s, ShengYiWu.JUE_YUAN))
 
     hua_hai_combins = list(itertools.combinations(hua_hai, 2))
     qian_yan_combins = list(itertools.combinations(qian_yan, 2))
     chen_lun_combins = list(itertools.combinations(chen_lun, 2))
     shui_xian_combins = list(itertools.combinations(shui_xian, 2))
 
+    jue_yuan_4 = list(itertools.combinations(jue_yuan, 4))
+
     all_combins = hua_hai_combins + qian_yan_combins + \
         chen_lun_combins + shui_xian_combins
     return [(l[0] + l[1])
-                    for l in list(itertools.combinations(all_combins, 2))]
+                    for l in list(itertools.combinations(all_combins, 2))] + jue_yuan_4
 
 
 def calculate_score_callback(combine : list[ShengYiWu], has_fu_fu = True, has_lei_shen = False):
@@ -51,8 +56,10 @@ def calculate_score_callback(combine : list[ShengYiWu], has_fu_fu = True, has_le
 
     extra_hp_bonus = {
         "专武": 0.16,
-        "四命保底两个e": 0.2,
     }
+
+    if ming_zuo_num >= 4:
+        extra_hp_bonus["四命保底两个e"] = 0.2
 
     common_elem_bonus = {
         "专武": 0.2,
@@ -97,12 +104,6 @@ def calculate_score_callback(combine : list[ShengYiWu], has_fu_fu = True, has_le
     if crit_rate < 0.70:
         return None
 
-
-    energe_recharge *= 100
-    energe_recharge = round(energe_recharge, 1)
-    if energe_recharge < 120:
-        return None
-
     syw_names = [p.name for p in combine]
     # print(syw_names)
     name_count = {i: syw_names.count(i) for i in syw_names}
@@ -120,7 +121,18 @@ def calculate_score_callback(combine : list[ShengYiWu], has_fu_fu = True, has_le
             hp_per += 0.2
         elif n == ShengYiWu.CHEN_LUN:
             elem_bonus += 0.15
+        elif n == ShengYiWu.JUE_YUAN:
+            if name_count[n] >= 4:
+                energe_recharge += 0.2 # 绝缘2件套
 
+    energe_recharge *= 100
+    energe_recharge = round(energe_recharge, 1)
+    if energe_recharge < 120:
+        return None
+    
+    if ShengYiWu.JUE_YUAN in name_count and name_count[ShengYiWu.JUE_YUAN] >= 4:
+        extra_q_bonus["绝缘4件套"] = min(energe_recharge / 4 / 100, 0.75)
+    
     all_hp = int(base_hp * (1 + hp_per + sum(extra_hp_bonus.values()))) + hp + 4780
 
     # 扣除切人时间，按一轮循环中11次协同攻击算
@@ -138,16 +150,24 @@ def calculate_score_callback(combine : list[ShengYiWu], has_fu_fu = True, has_le
     qx_damage_crit = qx_damage * crit_damage
     qx_damage_expect = qx_damage * (1 + crit_rate * (crit_damage - 1))
 
-    extra_qx_damage = all_hp * 14 / 100 * q_elem_bonus * 5
-    extra_qx_damage_crit = extra_qx_damage * crit_damage
-    extra_qx_damage_expect = extra_qx_damage * \
-        (1 + crit_rate * (crit_damage - 1))
-    
-    po_ju_shi_elem_bonus = (elem_bonus - common_elem_bonus["夜兰大招平均增伤"] + po_ju_shi_avg_bonus)
-    po_ju_shi_damage = all_hp * 20.84 / 100 * po_ju_shi_elem_bonus * 1.56 * 5
-    po_ju_shi_damage_crit = po_ju_shi_damage * crit_damage
-    po_ju_shi_damage_expect = po_ju_shi_damage * \
-        (1 + crit_rate * (crit_damage - 1))
+    if ming_zuo_num >= 2:
+        extra_qx_damage = all_hp * 14 / 100 * q_elem_bonus * 5
+        extra_qx_damage_crit = extra_qx_damage * crit_damage
+        extra_qx_damage_expect = extra_qx_damage * \
+            (1 + crit_rate * (crit_damage - 1))
+    else:
+        extra_qx_damage_crit = 0
+        extra_qx_damage_expect = 0
+
+    if ming_zuo_num == 6:
+        po_ju_shi_elem_bonus = (elem_bonus - common_elem_bonus["夜兰大招平均增伤"] + po_ju_shi_avg_bonus)
+        po_ju_shi_damage = all_hp * 20.84 / 100 * po_ju_shi_elem_bonus * 1.56 * 5
+        po_ju_shi_damage_crit = po_ju_shi_damage * crit_damage
+        po_ju_shi_damage_expect = po_ju_shi_damage * \
+            (1 + crit_rate * (crit_damage - 1))
+    else:
+        po_ju_shi_damage_crit = 0
+        po_ju_shi_damage_expect = 0
 
     # e技能只有一个能吃到夜兰自身大招增伤，开局两个都吃不到，但不考虑
     e_damage = all_hp * 45.2 / 100 * (elem_bonus * 2 - common_elem_bonus["夜兰大招平均增伤"])
