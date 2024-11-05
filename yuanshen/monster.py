465ab3e5bf16e2c05510839d68dc66a02ab60dae
Lutts <lutts.cao@gmail.com>
2024-11-05 16:22:14 +0800

    more doc

--- yuanshen/monster.py
+++ yuanshen/monster.py
@@ -94,7 +94,7 @@ class Monster:
     
 
 if __name__ == '__main__':
-    monster = Monster(level=10, character_level=90)
-    monster.add_jian_kang(0.9)
-    #monster.add_jian_fang(0.3)
+    monster = Monster(level=100, character_level=90)
+    monster.add_jian_kang(0.3)
+    monster.add_jian_fang(0.3)
     print(monster.attacked(1))
